b0ad427bda7287071c6277f3d1b7850c8b2146d4
csh <458761603@qq.com>
2023-03-31 14:00:30 +0800

    add install script & config.toml

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "install rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+
+echo "build helix"
+cargo install --locked --path helix-term
+ln -s $PWD/runtime ~/.config/helix/runtime
+cp config.toml ~/.config/helix/
+
+echo "install rust-analyzer"
+rustup component add rust-analyzer
